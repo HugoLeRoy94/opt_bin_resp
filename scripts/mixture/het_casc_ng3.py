5a207c900c19b37fa89f450c6345bfc332562aa8
HugoLeRoy94 <leroyhugo@yahoo.fr>
2026-06-17 10:19:23 +0200

    het 5 and 3 with less ligands

--- scripts/mixture/het_casc_ng3.py
+++ scripts/mixture/het_casc_ng3.py
@@ -38,7 +38,7 @@ config = RunConfig(
     # --- Presence (hierarchical sampler) ---
     n_presence_blocks      = 1,
     mu_sources             = 1,
-    mu_ligands_per_source  = np.repeat(np.random.randint(30, 81, N_RUNS), _NS).tolist(),
+    mu_ligands_per_source  = np.repeat(np.random.randint(10, 20, N_RUNS), _NS).tolist(),
     block_shared_conc_mean = False,
 
     # --- Interface model ---
